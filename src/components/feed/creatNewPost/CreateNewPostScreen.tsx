10a5aa1956f6f6e93d2a923e698ce3f2f1105d0a
Darya Svirsky <svirskydarya@gmail.com>
2023-03-21 00:41:38 +0200

    new post page

--- src/components/feed/creatNewPost/CreateNewPostScreen.tsx
+++ src/components/feed/creatNewPost/CreateNewPostScreen.tsx
@@ -1,10 +1,20 @@
-import { Tab, TabView } from "@rneui/themed";
+import { Input, ListItem, Tab, TabView } from "@rneui/themed";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 import { Colors } from "../../../theme/Colors";
 
 export default function CreateNewPostScreen() {
   const [currentTab, setCurrentTab] = React.useState(0);
+  const [locationsOpen, setLocationsOpen] = React.useState(
+    false as boolean
+  );
 
   return (
     <View style={{ flex: 1 }}>
@@ -33,11 +43,62 @@ export default function CreateNewPostScreen() {
         value={currentTab}
         onChange={setCurrentTab}
         animationType="spring"
+        containerStyle={{ flex: 1 }}
       >
-        <TabView.Item>
-          <Text>Location</Text>
+        <TabView.Item style={{ flex: 1 }}>
+          <ScrollView>
+            <View
+              style={{ width: "100%", alignItems: "center", marginBottom: 30 }}
+            >
+              <TouchableOpacity
+                style={{
+                  backgroundColor: "#aaa",
+                  width: "40%",
+                  aspectRatio: 1,
+                  borderRadius: 15,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  marginTop: 30,
+                }}
+              >
+                <Text
+                  style={{ fontWeight: "600", fontSize: 18, color: "white" }}
+                >
+                  + Add Photo
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <ListItem topDivider>
+              <ListItem.Content>
+                <ListItem.Title>Description</ListItem.Title>
+                <TextInput
+                  placeholder="Write description..."
+                  style={{ marginTop: 10, fontSize: 16, margin: 5 }}
+                  multiline
+                  underlineColorAndroid={"transparent"}
+                  //onChangeText={(text) => setData({ ...data, userEmail: text })}
+                  //value={data.userEmail}
+                />
+              </ListItem.Content>
+            </ListItem>
+            <ListItem.Accordion
+              topDivider
+              bottomDivider
+              isExpanded={locationsOpen}
+              onPress={() => setLocationsOpen(!locationsOpen)}
+              content={
+                <ListItem.Content>
+                  <ListItem.Title>Location</ListItem.Title>
+                </ListItem.Content>
+              }
+            >
+              <Input></Input>
+            </ListItem.Accordion>
+            <ListItem.Chevron />
+          </ScrollView>
         </TabView.Item>
-        <TabView.Item>
+        <TabView.Item style={{ flex: 1 }}>
           <Text>Route</Text>
         </TabView.Item>
       </TabView>
