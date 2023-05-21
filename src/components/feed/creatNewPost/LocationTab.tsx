73ea539e925678fba13a9651ed658ec07d9d4d6f
Darya Svirsky <svirskydarya@gmail.com>
2023-05-21 22:53:01 +0300

    move location to tab file

---
+++ src/components/feed/creatNewPost/LocationTab.tsx
@@ -0,0 +1,111 @@
+import { Image, Input, ListItem } from "@rneui/themed";
+import React, { forwardRef, useImperativeHandle, useState } from "react";
+import { Text, TouchableOpacity, View, TextInput } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { SaveLocationData, saveLocation } from "../../../actions/feedActions";
+
+const LocationTab = forwardRef((props, ref) => {
+  const [locationsOpen, setLocationsOpen] = React.useState(false as boolean);
+
+  const [data, setData] = useState({
+    description: "",
+    locationLat: "100",
+    locationLong: "200",
+    postGen: "0",
+    cities: "",
+  } as SaveLocationData);
+
+  const [image, setImage] = React.useState(
+    null as ImagePicker.ImagePickerAsset
+  );
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0]);
+    }
+  };
+
+  useImperativeHandle(ref, () => ({
+    save() {
+      return saveLocation(data, image);
+    },
+  }));
+
+  return (
+    <KeyboardAwareScrollView>
+      <View style={{ width: "100%", alignItems: "center", marginBottom: 30 }}>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#aaa",
+            width: "50%",
+            aspectRatio: 1,
+            borderRadius: 15,
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: 30,
+          }}
+          onPress={pickImage}
+        >
+          {image ? (
+            <Image
+              source={{
+                uri: image?.uri,
+              }}
+              style={{
+                width: "100%",
+                aspectRatio: 1,
+                borderRadius: 15,
+                resizeMode: "cover",
+              }}
+            />
+          ) : (
+            <Text style={{ fontWeight: "600", fontSize: 18, color: "white" }}>
+              + Add Photo
+            </Text>
+          )}
+        </TouchableOpacity>
+      </View>
+
+      <ListItem topDivider>
+        <ListItem.Content>
+          <ListItem.Title>Description</ListItem.Title>
+          <TextInput
+            placeholder="Write description..."
+            style={{ marginTop: 10, fontSize: 16, margin: 5 }}
+            multiline
+            underlineColorAndroid={"transparent"}
+            onChangeText={(text) => setData({ ...data, description: text })}
+            value={data.description}
+          />
+        </ListItem.Content>
+      </ListItem>
+      <ListItem.Accordion
+        topDivider
+        bottomDivider
+        isExpanded={locationsOpen}
+        onPress={() => setLocationsOpen(!locationsOpen)}
+        content={
+          <ListItem.Content>
+            <ListItem.Title>Location</ListItem.Title>
+          </ListItem.Content>
+        }
+      >
+        <Input></Input>
+      </ListItem.Accordion>
+    </KeyboardAwareScrollView>
+  );
+});
+
+export default LocationTab;
